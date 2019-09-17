8d30670869fe0f13d892a460b35cf8fd9032a9e0
vladislav-vasilyev <vladislav.vasilyev@mifort.org>
2019-09-17 19:25:56 +0300

    Added necessary key prop to Field types

--- src/constants/attributes.js
+++ src/constants/attributes.js
@@ -10,34 +10,42 @@ import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
 
 export const FIELD_TYPES = {
   text: {
+    key: 0,
     title: 'Text',
     iconComponent: <ImageAspectRatioOutlinedIcon />,
   },
   textArea: {
+    key: 1,
     title: 'Text Area',
     iconComponent: <SubjectIcon />,
   },
   selectList: {
+    key: 2,
     title: 'Select List',
     iconComponent: <ViewColumnOutlinedIcon />,
   },
   boolean: {
+    key: 3,
     title: 'Boolean',
     iconComponent: <CheckBoxOutlinedIcon />,
   },
   number: {
+    key: 4,
     title: 'Number',
     iconComponent: <ViewQuiltOutlinedIcon />,
   },
   date: {
+    key: 5,
     title: 'Date',
     iconComponent: <CalendarTodayOutlinedIcon />,
   },
   time: {
+    key: 6,
     title: 'Time',
     iconComponent: <WatchLaterOutlinedIcon />,
   },
   location: {
+    key: 7,
     title: 'Location',
     iconComponent: <LocationOnOutlinedIcon />,
   },
