b08756f0b3fc2a32287ceb278289d639f0674611
Rivka Tzur <rivkat@linnovate.net>
2020-05-19 16:18:23 +0300

    trialMutationUpdate

---
+++ src/components/TrialContext/TrialForm/utils/trialMutationUpdate.js
@@ -0,0 +1,68 @@
+import gql from 'graphql-tag';
+import { TRIAL_MUTATION } from '../../../../constants/base';
+
+export default (trial) => {
+  const key = trial.key ? trial.key : `${trial.experimentId}_${Date.now()}`;
+
+  return gql`mutation {
+        ${TRIAL_MUTATION}(
+          action: "update",
+          key:"${key}",
+          uid:"${localStorage.getItem('uid')}"
+          experimentId:"${trial.experimentId}"
+          ${trial.id ? `id:"${trial.id}"` : ''},
+          ${trial.name ? `name:"${trial.name}"` : ''},
+          ${trial.status ? `status:"${trial.status}"` : ''},
+          trialSetKey:"${trial.trialSetKey}",
+          ${trial.numberOfDevices ? `numberOfDevices:"${trial.numberOfDevices}"` : ''},
+          ${trial.state ? `state:"${trial.state}"` : ''}
+          ${trial.properties ? `properties:${JSON.stringify(trial.properties)
+    .replace(/"key":/g, 'key:')
+    .replace(/"val":/g, 'val:')}` : ''}
+          ${trial.entities ? `entities: ${JSON.stringify(trial.entities)
+    .replace(/"key":/g, 'key:')
+    .replace(/"val":/g, 'val:')
+    .replace(/"type":/g, 'type:')
+    .replace(/"typeKey":/g, 'typeKey:')
+    .replace(/"properties":/g, 'properties:')}` : ''},
+        ${trial.deployedEntities ? `deployedEntities: ${JSON.stringify(trial.deployedEntities)
+    .replace(/"key":/g, 'key:')
+    .replace(/"val":/g, 'val:')
+    .replace(/"type":/g, 'type:')
+    .replace(/"typeKey":/g, 'typeKey:')
+    .replace(/"properties":/g, 'properties:')}` : ''}
+            )
+            {
+              key
+              created
+              status
+              id
+              name
+              trialSetKey
+              numberOfDevices
+              state
+              properties {
+                key
+                val
+              }
+              entities {
+                key
+                typeKey
+                type
+                properties {
+                  key
+                  val
+                }
+              }
+              deployedEntities {
+                key
+                typeKey
+                type
+                properties {
+                  key
+                  val
+                }
+              }
+            }
+      }`;
+};
