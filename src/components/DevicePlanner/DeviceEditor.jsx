7e7d7edd5b89ae411c6fcce633ebae87bb8ac905
Eran Geva <eran.geva@gmail.com>
2020-05-27 18:33:17 +0300

    leaflet panel 88%

--- src/components/DevicePlanner/DeviceEditor.jsx
+++ src/components/DevicePlanner/DeviceEditor.jsx
@@ -191,9 +191,8 @@ export const DeviceEditor = ({ devices, setDevices }) => {
             </LeafletMap>
             <Paper
                 style={{
-                    position: 'absolute', height: '80%', maxHeight: '75%', overflow: 'auto',
-                    top: 0, width: '30%', left: 0, bottom: 50,
-                    justifyContent: 'center', alignItems: 'center', zIndex: 1000
+                    position: 'absolute', height: '88%', overflow: 'auto', top: 0, width: '30%',
+                    left: 0, justifyContent: 'center', alignItems: 'center', zIndex: 1000
                 }}
             >
                 <div
