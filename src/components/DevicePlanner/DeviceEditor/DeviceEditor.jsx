91852e26be58c71c1aa817811021c327b9fc8960
Eran Geva <eran.geva@gmail.com>
2020-06-21 17:29:17 +0300

    image

--- src/components/DevicePlanner/DeviceEditor/DeviceEditor.jsx
+++ src/components/DevicePlanner/DeviceEditor/DeviceEditor.jsx
@@ -1,6 +1,6 @@
 import { Button, InputLabel, List, Paper, Slider, Switch } from '@material-ui/core';
 import React, { useRef, useEffect } from 'react';
-import { Map as LeafletMap, Polyline, TileLayer, LayersControl } from "react-leaflet";
+import { Map as LeafletMap, Polyline, TileLayer, LayersControl, ImageOverlay } from "react-leaflet";
 import { DeviceMarker } from './DeviceMarker';
 import { DeviceRow } from './DeviceRow';
 import { JsonStreamer } from './JsonStreamer';
@@ -179,6 +179,12 @@ export const DeviceEditor = ({ devices, setDevices }) => {
                         <TileLayer
                             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                    </LayersControl.BaseLayer>
+                    <LayersControl.BaseLayer name="Image">
+                        <ImageOverlay
+                            url="https://cdn.vox-cdn.com/thumbor/HKALidP1Nm7vvd6GrsLmUCSVlEw=/0x0:2048x2732/1200x800/filters:focal(540x2092:866x2418)/cdn.vox-cdn.com/uploads/chorus_image/image/52202887/super_mario_run_ipad_screenshot_01_2048.0.jpeg"
+                            bounds={[[31.8, 34.2], [32.3, 35.2]]}
                         />
                     </LayersControl.BaseLayer>
                 </LayersControl>
