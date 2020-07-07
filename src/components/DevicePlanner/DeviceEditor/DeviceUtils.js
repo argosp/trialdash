4a01f2dcc1867d3757f93129a14f006c95818644
Eran Geva <eran.geva@gmail.com>
2020-07-07 16:07:22 +0300

    findDevicesChanged

--- src/components/DevicePlanner/DeviceEditor/DeviceUtils.js
+++ src/components/DevicePlanner/DeviceEditor/DeviceUtils.js
@@ -4,11 +4,16 @@ export const getTypeLocationProp = (deviceType) => {
     return locationProp.key;
 }
 
+export const getDeviceLocationProp = (device, deviceType) => {
+    const key = getTypeLocationProp(deviceType);
+    const prop = device.properties.find(pr => pr.key === key);
+    if (!prop || !prop.val || !prop.val.coordinates) return undefined;
+    return prop;
+}
+
 export const getDeviceLocation = (device, deviceType) => {
-    const locationProp = getTypeLocationProp(deviceType);
-    const pos = device.properties.find(pr => pr.key === locationProp);
-    if (!pos || !pos.val || !pos.val.coordinates) return undefined;
-    return pos.val.coordinates;
+    const prop = getDeviceLocationProp(device, deviceType);
+    if (prop) return prop.val.coordinates;
 }
 
 export const setDeviceLocation = (device, deviceType, newLocation) => {
@@ -23,3 +28,26 @@ export const setDeviceLocation = (device, deviceType, newLocation) => {
     });
 }
 
+export const sortDevices = (deviceTypes) => {
+    deviceTypes.sort((a, b) => (a.name + ";" + a.key).localeCompare(b.name + ";" + b.key));
+    deviceTypes.forEach(devType => {
+        devType.items.sort((a, b) => (a.name + ";" + a.key).localeCompare(b.name + ";" + b.key));
+    });
+    return deviceTypes;
+}
+
+export const findDevicesChanged = (oldDeviceTypes, newDeviceTypes) => {
+    let ret = [];
+    newDeviceTypes.forEach(newDevType => {
+        const oldDevType = oldDeviceTypes.find(ty => ty.key === newDevType.key);
+        if (oldDevType && oldDevType.items && newDevType.items) {
+            newDevType.items.forEach(newDev => {
+                const oldDev = oldDevType.items.find(d => d.key === newDev.key);
+                if (oldDev && JSON.stringify(oldDev) !== JSON.stringify(newDev)) {
+                    ret.push({ dev: newDev, type: newDevType });
+                }
+            })
+        }
+    });
+    return ret;
+}
