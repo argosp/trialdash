ead3b47dcfb623c6083ecb1a569bd169be613d39
erasta <eran@kaplanopensource.co.il>
2020-08-11 18:22:35 +0300

    devicesTrialQuery inside

--- src/components/DevicePlanner/index.jsx
+++ src/components/DevicePlanner/index.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 import { withApollo } from 'react-apollo';
 import { withRouter } from 'react-router-dom';
 import { compose } from 'recompose';
-import deviceTypesQuery from '../DeviceContext/utils/deviceTypeQuery';
 import { DeviceEditor } from './DeviceEditor/DeviceEditor';
 import { styles } from './styles';
-// import devicesQuery from '../TrialContext/TrialForm/utils/devicesTrialQuery';
 import { getTypeLocationProp, getDeviceLocationProp, sortDevices, findDevicesChanged } from './DeviceEditor/DeviceUtils';
+import deviceTypesQuery from '../DeviceContext/utils/deviceTypeQuery';
+import devicesTrialQuery from './utils/devicesTrialQuery';
 
-const DevicePlanner = ({ client, trial, match, updateLocation, getDevicesByTrial }) => {
+const DevicePlanner = ({ client, trial, match, updateLocation }) => {
     const [devices, setDevices] = React.useState([]);
 
     console.log('props', client, trial, match);
@@ -22,10 +22,14 @@ const DevicePlanner = ({ client, trial, match, updateLocation, getDevicesByTrial
                 const deviceTypes = dataType.data.deviceTypes.filter(devtype => devtype.name && getTypeLocationProp(devtype));
                 console.log('deviceTypes', deviceTypes);
                 deviceTypes.forEach(devtype => {
-                    getDevicesByTrial(experimentId, devtype.key)
+                    client.query({ query: devicesTrialQuery(experimentId, devtype.key, undefined) })
+                        .then(dataDev => {
+                            console.log('devices undef', dataDev.data.devices);
+                        });
+                    client.query({ query: devicesTrialQuery(experimentId, devtype.key, trial.key) })
                         .then(dataDev => {
                             devtype.items = dataDev.data.devices;
-                            console.log('devices', devtype.items);
+                            console.log('devices trial', devtype.items);
                             newdevs.push(devtype);
                             if (newdevs.length === deviceTypes.length) {
                                 sortDevices(newdevs);
--- src/components/TrialContext/TrialForm/TrialDevices/index.jsx
+++ src/components/TrialContext/TrialForm/TrialDevices/index.jsx
@@ -126,18 +126,18 @@ class TrialDevices extends React.Component {
     });
   };
 
- getDevicesByTrial = (experimentId, devtypeKey)=>{
-   console.log('getDevicesByTrial function ',experimentId, devtypeKey, this.props.trial);
-  const { client, trial } = this.props;  
-  // return devicesTrialQuery(experimentId, devtypeKey, trial.key);
- return client.query({ query: devicesTrialQuery(experimentId, devtypeKey, trial.key) })
-  // .then(devicesData => {
-    // const devices = devicesData.data.devices;
-    //  console.log('devices from devicesTrialQuery',devices);
-    // //  devices: groupBy(devices, 'key'),
-    //  return devices;
-  // })
- }
+//  getDevicesByTrial = (experimentId, devtypeKey)=>{
+//    console.log('getDevicesByTrial function ',experimentId, devtypeKey, this.props.trial);
+//   const { client, trial } = this.props;
+//   // return devicesTrialQuery(experimentId, devtypeKey, trial.key);
+//  return client.query({ query: devicesTrialQuery(experimentId, devtypeKey, trial.key) })
+//   // .then(devicesData => {
+//     // const devices = devicesData.data.devices;
+//     //  console.log('devices from devicesTrialQuery',devices);
+//     // //  devices: groupBy(devices, 'key'),
+//     //  return devices;
+//   // })
+//  }
   render() {
     const {
       classes,
@@ -245,7 +245,7 @@ class TrialDevices extends React.Component {
               updateLocation={this.updateLocation}
               trial={trial}
               entities={trial[trial.status === 'deploy' ? 'deployedEntities' : 'entities'].map(e => e.key)}
-              getDevicesByTrial= {this.getDevicesByTrial}
+            //   getDevicesByTrial= {this.getDevicesByTrial}
             />
           }
         </TabPanel>
