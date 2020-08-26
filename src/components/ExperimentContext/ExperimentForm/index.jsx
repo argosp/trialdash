745ba3ff7f8f7da2597f4d3a3945a7b7b388288b
Yehudit <yehuditg@linnovate.net>
2020-08-26 16:58:04 +0300

    fix add row in table

--- src/components/ExperimentContext/ExperimentForm/index.jsx
+++ src/components/ExperimentContext/ExperimentForm/index.jsx
@@ -44,26 +44,7 @@ class ExperimentForm extends React.Component {
 
     },
     isStartDatePickerOpen: false,
-    isEndDatePickerOpen: false,
-    columns: [
-      { title: 'Image', field: 'image',
-       render: rowData => <img src={rowData.imageUrl} style={{width: 50, borderRadius: '50%'}}/>,
-       editComponent: props => (
-        <input
-          type="file"
-          value={props.value}
-          onChange={e => props.onChange(e.target.value)}
-        />
-      )},
-      { title: 'Image name', field: 'imageName' },
-      { title: 'Bounds (x,y)', field: 'bounds' },
-      { title: 'Scale', field: 'scale', type: 'numeric' },
-
-    ],
-    data: [
-      { imageUrl: "https://d33wubrfki0l68.cloudfront.net/ca0061c3c33c88b2b124e64ad341e15e2a17af49/c8765/images/alligator-logo3.svg",
-       imageName: 'fdsaf',bounds:'3,5', scale: 1987 }
-    ],
+    isEndDatePickerOpen: false
   };
 
   
@@ -301,51 +282,7 @@ class ExperimentForm extends React.Component {
               </Map> */}
 
               {/* //display table */}
-              <MapsEditTable/>
-              {/* <div style={{ maxWidth: "100%" }}>
-              <MaterialTable
-                title="Images and locations"
-                columns={columns}
-                data={data}
-                editable={{
-                  onRowAdd: (newData) =>
-                    new Promise((resolve) => {
-                      setTimeout(() => {
-                        resolve();
-                        this.setState((prevState) => {
-                          const data = [...prevState.data];
-                          data.push(newData);
-                          return { ...prevState, data };
-                        });
-                      }, 600);
-                    }),
-                  onRowUpdate: (newData, oldData) =>
-                    new Promise((resolve) => {
-                      setTimeout(() => {
-                        resolve();
-                        if (oldData) {
-                          this.setState((prevState) => {
-                            const data = [...prevState.data];
-                            data[data.indexOf(oldData)] = newData;
-                            return { ...prevState, data };
-                          });
-                        }
-                      }, 600);
-                    }),
-                  onRowDelete: (oldData) =>
-                    new Promise((resolve) => {
-                      setTimeout(() => {
-                        resolve();
-                        this.setState((prevState) => {
-                          const data = [...prevState.data];
-                          data.splice(data.indexOf(oldData), 1);
-                          return { ...prevState, data };
-                        });
-                      }, 600);
-                    }),
-                }}
-              />
-              </div> */}
+              <MapsEditTable onConfirm = {() => this.submitExperiment(formObject)}/>
             </Grid>
           </Grid>
         </form>
--- src/components/ExperimentContext/ExperimentForm/utils/mapsEditTable.js
+++ src/components/ExperimentContext/ExperimentForm/utils/mapsEditTable.js
@@ -1,74 +1,91 @@
-import React, { useState, useEffect } from 'react';
+import React, {
+  useState,
+  useEffect
+} from 'react';
 import MaterialTable from 'material-table';
-export default function MapsEditTable() {
-    //get porps
-  const [data, setData] = useState([{ imageUrl: "",imageName: 'fdsaf',bounds:'3,5', scale: 1987 }],);
-  const columns = [
-    { title: 'Image', field: 'image',
-     render: rowData => <img src={rowData.image || rowData.imageUrl || ''} style={{width: 50, borderRadius: '50%'}}/>,
-     editComponent: props => (
-      <input
-        type="file"
-        value={props.value}
-        onChange={e => {onChangeFile(e)}} //props.onChange(e.target.files[0])
-        //check value to save (what type?), value to display as blob
-      />
-    )},
-    { title: 'Image name', field: 'imageName' },
-    { title: 'Bounds (x,y)', field: 'bounds' },
-    { title: 'Scale', field: 'scale', type: 'numeric' }];
+export default function MapsEditTable( props ) {
+  //get porps
+  const [data, setData] = useState([{
+    imageUrl: "",
+    imageName: 'fdsaf',
+    bounds: '3,5',
+    scale: 1987
+  }], );
+  const columns = [{
+      title: 'Image',
+      field: 'image',
+      render: rowData => < img src = {rowData.image || rowData.imageUrl || '' } style = {{width: '100%'}}/>,
+      editComponent: props => (<input type = "file" value = { props.value} onChange = {e => { onChangeFile(e) }} />) //check value to save (what type?), value to display as blob
+    },
+    {
+      title: 'Image name',
+      field: 'imageName'
+    },
+    {
+      title: 'Bounds (x,y)',
+      field: 'bounds'
+    },
+    {
+      title: 'Scale',
+      field: 'scale',
+      type: 'numeric'
+    }
+  ];
   let imageToDisplay;
-  
-  // useEffect(() => {
-  //     let mounted = true;
-  //     if(mounted){
-  //   console.log('data in useEffect ',data);
-  //     }
-  //     return () => mounted = false;
 
-  // },[data]);
+  useEffect(() => {
+    debugger
+      let mounted = true;
+      if(mounted){
+        console.log('data in useEffect ',data);
+        // props.onConfirm();
+     }
+      return () => mounted = false;
 
-  
-  const onChangeFile = (e)=> {  
-        const imageBlob = window.URL.createObjectURL(e.target.files[0]);
-        console.log('imageToDisplay ',imageBlob)
-         // URL.revokeObjectURL(imageToDisplay);
-         imageToDisplay = imageBlob;
-        // return imageToDisplay;
-        
+  },[data]);
+
+
+  const onChangeFile = (e) => {
+    const imageBlob = window.URL.createObjectURL(e.target.files[0]);
+    console.log('imageToDisplay ', imageBlob)
+    // URL.revokeObjectURL(imageToDisplay);
+    imageToDisplay = imageBlob;
   }
-  const handleRowUpdate = (newData, oldData)=>  {
-    console.log('handleRowUpdate ',newData,oldData);
-    if(imageToDisplay != ''){
-      newData.imageUrl = imageToDisplay;
+  const setImage = (newData) =>{
+    let updatedObj = newData;
+    if (imageToDisplay != '') {
+      updatedObj.imageUrl = imageToDisplay;
       imageToDisplay = '';
     }
+    return updatedObj;
+  }
+  const handleRowUpdate = (newData, oldData) => {
+    const updatedObj = setImage(newData);
+    console.log('handleRowUpdate ', updatedObj, oldData);
     const dataUpdate = [...data];
-    dataUpdate[data.indexOf(oldData)] = newData;
+    dataUpdate[data.indexOf(oldData)] = updatedObj;
     setData([...dataUpdate]);
- }
- const handleRowAdd = (newData)=>  {
-    console.log('handleRowAdd ',newData);
-    const dataUpdate = [...data];
-    dataUpdate.push = newData;
-    setData([...dataUpdate]);
- }
+  }
+  const handleRowAdd = (newData) => {
+    const updatedObj = setImage(newData);
+    console.log('handleRowAdd ', updatedObj);
+    setData([...data, updatedObj]);
+  }
 
- const handleRowDelete = (oldData)=>  {
-    console.log('handleRowDelete ',oldData);
+  const handleRowDelete = (oldData) => {
+    console.log('handleRowDelete ', oldData);
     const dataUpdate = [...data];
     dataUpdate.splice(data.indexOf(oldData), 1);
     setData([...dataUpdate]);
- }
+  }
 
-  return (
-    <div style={{ maxWidth: "100%" }}>
-    {/* <button onClick={() => setCount(count + 1)}> */}
-    <MaterialTable
-      title="Images and locations"
-      columns={columns}
-      data={data}
-      editable={{
+  return ( 
+    <div style = {{maxWidth: "100%"}} >
+    < MaterialTable title = "Images and locations"
+    columns = { columns }
+    data = { data }
+    editable = {
+      {
         onRowAdd: (newData) =>
           new Promise((resolve) => {
             setTimeout(() => {
@@ -77,10 +94,11 @@ export default function MapsEditTable() {
             }, 600);
           }),
         onRowUpdate: (newData, oldData) =>
-          new Promise((resolve) => {
+          new Promise((resolve,reject) => {
+            props.onConfirm();
             setTimeout(() => {
               if (oldData) {
-               handleRowUpdate(newData,oldData);
+                handleRowUpdate(newData, oldData);
               }
               resolve();
             }, 600)
@@ -92,11 +110,8 @@ export default function MapsEditTable() {
               resolve();
             }, 600);
           }),
-      }}
-    />
-    </div>
-
-    
-     
+      }
+    }
+    /> </div>
   );
 }
