a50689122a82315875cf782b1b0d83a58ec65d56
Eran Geva <eran@kaplanopensource.co.il>
2020-10-25 15:13:56 +0200

    selected control point

--- src/components/DevicePlanner/MarkedPoint.jsx
+++ src/components/DevicePlanner/MarkedPoint.jsx
@@ -3,15 +3,26 @@ import { Marker, Popup } from "react-leaflet";
 import { divIcon } from 'leaflet';
 import { renderToStaticMarkup } from 'react-dom/server';
 
-export const MarkedPoint = ({ location, dragLocation, setLocation }) => {
-    const strLoc = JSON.stringify(location.map(l => Math.round(l * 1e9) / 1e9)).replace(',', ', ');
+export const MarkedPoint = (props) => {
+    const { location, dragLocation, setLocation } = props;
+    const formatLoc = (loc) => {
+        return JSON.stringify(loc.map(l => Math.round(l * 1e9) / 1e9)).replace(',', ', ');
+    }
     return (
         <Marker key={location}
             position={location}
-            title={strLoc}
+            title={formatLoc(location)}
             draggable={true}
-            ondrag={(e) => dragLocation && dragLocation(Object.values(e.target.getLatLng()))}
-            ondragend={(e) => setLocation && setLocation(Object.values(e.target.getLatLng()))}
+            ondrag={(e) => {
+                if (dragLocation) {
+                    dragLocation(Object.values(e.target.getLatLng()))
+                }
+            }}
+            ondragend={(e) => {
+                if (setLocation) {
+                    setLocation(Object.values(e.target.getLatLng()))
+                }
+            }}
             icon={divIcon({
                 iconSize: [20, 20],
                 html: renderToStaticMarkup(
@@ -21,9 +32,10 @@ export const MarkedPoint = ({ location, dragLocation, setLocation }) => {
                 ),
                 iconAnchor: [6, 9]
             })}
+            {...props}
         >
-            <Popup>
-                {strLoc}
+            <Popup permanent>
+                {formatLoc(location)}
             </Popup>
         </Marker >
     )
