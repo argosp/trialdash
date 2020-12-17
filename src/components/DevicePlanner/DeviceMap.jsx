a747cccecc465c720e9766e40a381b5b9f619640
Eran Geva <eran@kaplanopensource.co.il>
2020-12-17 12:18:09 +0200

    Layers menu

--- src/components/DevicePlanner/DeviceMap.jsx
+++ src/components/DevicePlanner/DeviceMap.jsx
@@ -1,34 +1,31 @@
 import React, { useState } from 'react';
-import { Map as LeafletMap } from "react-leaflet";
-import { TileLayer, LayersControl, ImageOverlay } from "react-leaflet";
+import {
+    Map as LeafletMap,
+    TileLayer,
+    LayersControl,
+    ImageOverlay,
+    LayerGroup
+} from "react-leaflet";
 import config from '../../config';
 
 const position = [32.081128, 34.779729];
 
 export const DeviceMap = ({ onClick, onMouseMove, onMouseOut, experimentDataMaps, children }) => {
-    console.log(experimentDataMaps)
     const mapElement = React.useRef(null);
+
     const mapAttrib = process.env.REACT_APP_MAP_ATTRIBUTION || '&copy; <a href="https://carto.com">Carto</a> contributors';
     const mapTileUrl = process.env.REACT_APP_MAP_URL || 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}.png';
     if (!process.env.REACT_APP_MAP_ATTRIBUTION || !process.env.REACT_APP_MAP_URL) {
         console.log('Getting map tileserver url from hardcoded:', mapTileUrl);
     }
 
-    const [mutedImages, setMutedImages] = useState({});
-
-    const setOverlay = (name, toOn) => {
-        const newMutedImages = Object.assign({}, mutedImages)
-        newMutedImages[name] = !toOn;
-        setMutedImages(newMutedImages);
-    }
+    const images = experimentDataMaps || [];
+    const embedded = images.filter(row => row.embedded);
+    const standalone = images.filter(row => !row.embedded);
 
     React.useEffect(() => {
         mapElement.current.leafletElement.invalidateSize();
-        mapElement.current.leafletElement.on('overlayadd', (e) => setOverlay(e.name, true));
-        mapElement.current.leafletElement.on('overlayremove', (e) => setOverlay(e.name, false));
     }, []);
-
-    let showMap = !experimentDataMaps || experimentDataMaps.length === 0 || experimentDataMaps.some(row => row.embedded);
 
     return (
         <LeafletMap
@@ -41,25 +38,38 @@ export const DeviceMap = ({ onClick, onMouseMove, onMouseOut, experimentDataMaps
             onMouseOut={onMouseOut}
         >
 
-            {
-                !showMap ? null :
-                    <TileLayer
-                        attribution={mapAttrib}
-                        url={mapTileUrl}
-                    />
-            }
-            <LayersControl position="topright">
+            <LayersControl position="topright" collapsed={false}>
                 {
-                    (experimentDataMaps || []).map(row => {
-                        return (
-                            <LayersControl.Overlay name={row.imageName} checked={!mutedImages[row.imageName]}>
-                                <ImageOverlay
-                                    url={config.url + '/' + row.imageUrl}
-                                    bounds={[[row.upper, row.left], [row.lower, row.right]]}
+                    !embedded.length ? null :
+                        <LayersControl.BaseLayer name="Map" checked={true}>
+                            <LayerGroup>
+                                <TileLayer
+                                    attribution={mapAttrib}
+                                    url={mapTileUrl}
                                 />
-                            </LayersControl.Overlay>
-                        )
-                    })
+                                {
+                                    embedded.map(row => (
+                                        <ImageOverlay
+                                            url={config.url + '/' + row.imageUrl}
+                                            bounds={[[row.upper, row.left], [row.lower, row.right]]}
+                                        />
+                                    ))
+                                }
+                            </LayerGroup>
+                        </LayersControl.BaseLayer>
+                }
+                {
+                    standalone.map((row, i) => (
+                        <LayersControl.BaseLayer
+                            name={row.imageName}
+                            checked={embedded.length === 0 && i === 0}
+                        >
+                            <ImageOverlay
+                                url={config.url + '/' + row.imageUrl}
+                                bounds={[[row.upper, row.left], [row.lower, row.right]]}
+                            />
+                        </LayersControl.BaseLayer>
+                    ))
                 }
             </LayersControl>
 
