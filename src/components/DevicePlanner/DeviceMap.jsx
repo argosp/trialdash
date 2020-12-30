0e62bc6226776e59000bb27f7097e0b52c9e5fe5
Eran Geva <eran@kaplanopensource.co.il>
2020-12-30 15:22:35 +0200

    Fix map not showing when no images

--- src/components/DevicePlanner/DeviceMap.jsx
+++ src/components/DevicePlanner/DeviceMap.jsx
@@ -10,14 +10,69 @@ import config from '../../config';
 
 const position = [32.081128, 34.779729];
 
-export const DeviceMap = ({ onClick, onMouseMove, onMouseOut, experimentDataMaps, children }) => {
-    const mapElement = React.useRef(null);
-
+const RealMapLayer = () => {
     const mapAttrib = process.env.REACT_APP_MAP_ATTRIBUTION || '&copy; <a href="https://carto.com">Carto</a> contributors';
     const mapTileUrl = process.env.REACT_APP_MAP_URL || 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}.png';
     if (!process.env.REACT_APP_MAP_ATTRIBUTION || !process.env.REACT_APP_MAP_URL) {
         console.log('Getting map tileserver url from hardcoded:', mapTileUrl);
     }
+
+    return (
+        <TileLayer
+            attribution={mapAttrib}
+            url={mapTileUrl}
+        />
+    )
+}
+
+const EmbeddedImageLayer = ({ image }) => (
+    <ImageOverlay
+        url={config.url + '/' + image.imageUrl}
+        bounds={[[image.upper, image.left], [image.lower, image.right]]}
+    />
+)
+
+const RealMapWithImagesLayer = ({ images }) => (
+    <>
+        <RealMapLayer />
+        {
+            images.map(row => (
+                <EmbeddedImageLayer image={row} />
+            ))
+        }
+    </>
+)
+
+const DeviceMapLayers = ({ embedded, standalone }) => {
+    if (!standalone.length) {
+        return <RealMapWithImagesLayer images={embedded} />
+    }
+    return (
+        <LayersControl position="topright" collapsed={false}>
+            {
+                !embedded.length ? null :
+                    <LayersControl.BaseLayer name="Map" checked={true}>
+                        <LayerGroup>
+                            <RealMapWithImagesLayer images={embedded} />
+                        </LayerGroup>
+                    </LayersControl.BaseLayer>
+            }
+            {
+                standalone.map((row, i) => (
+                    <LayersControl.BaseLayer
+                        name={row.imageName}
+                        checked={embedded.length === 0 && i === 0}
+                    >
+                        <EmbeddedImageLayer image={row} />
+                    </LayersControl.BaseLayer>
+                ))
+            }
+        </LayersControl>
+    )
+}
+
+export const DeviceMap = ({ onClick, onMouseMove, onMouseOut, experimentDataMaps, children }) => {
+    const mapElement = React.useRef(null);
 
     const images = experimentDataMaps || [];
     const embedded = images.filter(row => row.embedded);
@@ -37,42 +92,7 @@ export const DeviceMap = ({ onClick, onMouseMove, onMouseOut, experimentDataMaps
             onMouseMove={onMouseMove}
             onMouseOut={onMouseOut}
         >
-
-            <LayersControl position="topright" collapsed={false}>
-                {
-                    !embedded.length ? null :
-                        <LayersControl.BaseLayer name="Map" checked={true}>
-                            <LayerGroup>
-                                <TileLayer
-                                    attribution={mapAttrib}
-                                    url={mapTileUrl}
-                                />
-                                {
-                                    embedded.map(row => (
-                                        <ImageOverlay
-                                            url={config.url + '/' + row.imageUrl}
-                                            bounds={[[row.upper, row.left], [row.lower, row.right]]}
-                                        />
-                                    ))
-                                }
-                            </LayerGroup>
-                        </LayersControl.BaseLayer>
-                }
-                {
-                    standalone.map((row, i) => (
-                        <LayersControl.BaseLayer
-                            name={row.imageName}
-                            checked={embedded.length === 0 && i === 0}
-                        >
-                            <ImageOverlay
-                                url={config.url + '/' + row.imageUrl}
-                                bounds={[[row.upper, row.left], [row.lower, row.right]]}
-                            />
-                        </LayersControl.BaseLayer>
-                    ))
-                }
-            </LayersControl>
-
+            <DeviceMapLayers embedded={embedded} standalone={standalone} />
             {children}
         </LeafletMap>
     );
