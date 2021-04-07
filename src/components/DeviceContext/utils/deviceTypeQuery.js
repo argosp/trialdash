51947b07d5c49808fb524c3d92daff2ea0dd91ab
Rivka Tzur <rivkat@linnovate.net>
2021-04-07 16:14:55 +0300

    remove id

--- src/components/DeviceContext/utils/deviceTypeQuery.js
+++ src/components/DeviceContext/utils/deviceTypeQuery.js
@@ -3,14 +3,12 @@ import gql from 'graphql-tag';
 const deviceTypes = experimentId => gql`
  {
     deviceTypes(experimentId:"${experimentId}"){
-       id
        key
        name
        numberOfDevices
        properties{
            key
            type
-           id
            label
            description
            prefix
