c9fa39423c1292d18e0e066a6b37a54c36c8e77b
Eran Geva <eran.geva@gmail.com>
2021-08-05 12:54:07 +0300

    context menu for location button #203

--- src/components/EntityPlanner/EntityRow.jsx
+++ src/components/EntityPlanner/EntityRow.jsx
@@ -1,4 +1,4 @@
-import { IconButton, ListItem, ListItemText, Tooltip } from '@material-ui/core';
+import { IconButton, ListItem, ListItemText, Tooltip, Menu, MenuItem } from '@material-ui/core';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
 import React from 'react';
 
@@ -7,27 +7,54 @@ const formatEntityLocation = (loc, layer) => {
 }
 
 const EntityLocationButton = ({ entityLocation, isEntityOnLayer, entityLayerName, onDisableLocation }) => {
+    const [anchorEl, setAnchorEl] = React.useState(null);
     let tip = entityLocation.map(x => Math.round(x * 1e5) / 1e5).join(', ');
     if (!isEntityOnLayer) {
         tip = '(' + tip + ') on ' + entityLayerName;
     }
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
     return (
-        <Tooltip title={tip}>
-            <IconButton aria-label="Disable location" size="small"
-                onClick={(e) => {
-                    e.stopPropagation();
-                    e.preventDefault();
-                    onDisableLocation(e.shiftKey);
-                }}
-                onContextMenu={(e) => {
-                    e.stopPropagation();
-                    e.preventDefault();
-                    console.log(e);
-                }}
+        <>
+            <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
             >
-                <LocationOnIcon color={isEntityOnLayer ? "primary" : "inherit"} />
-            </IconButton>
-        </Tooltip>
+                <MenuItem onClick={() => {
+                    onDisableLocation(true);
+                    handleClose();
+                }}>
+                    Remove all (shift+click)
+                </MenuItem>
+                <MenuItem onClick={() => {
+                    onDisableLocation(false);
+                    handleClose();
+                }}>
+                    Remove this one (click)
+                </MenuItem>
+                <MenuItem onClick={handleClose}>Cancel</MenuItem>
+            </Menu>
+            <Tooltip title={tip}>
+                <IconButton aria-label="Disable location" size="small"
+                    onClick={(e) => {
+                        e.stopPropagation();
+                        e.preventDefault();
+                        onDisableLocation(e.shiftKey);
+                    }}
+                    onContextMenu={(e) => {
+                        e.stopPropagation();
+                        e.preventDefault();
+                        setAnchorEl(e.currentTarget);
+                    }}
+                >
+                    <LocationOnIcon color={isEntityOnLayer ? "primary" : "inherit"} />
+                </IconButton>
+            </Tooltip>
+        </>
     );
 };
 
