4a29e82b9c9a7eb98210eabf84b963a054c3f2a5
Yehudit <yehuditg@linnovate.net>
2021-09-30 13:46:40 +0300

    add functions

--- src/assets/Utils/index.js
+++ src/assets/Utils/index.js
@@ -2,8 +2,16 @@ export const getCurrentEntitsNameByStatus = (trial) => {
   return trial.status == "design" ? "entities" : "deployedEntities";
 };
 
-export const getEntitiesByEntitiesTypeKey = (trial, entitiesTypeKey ) => {
-  return trial.entities.filter(
-    (e) => e.entitiesTypeKey == entitiesTypeKey
-  );
+export const getEntitiesByEntitiesTypeKey = (trial, entitiesTypeKey) => {
+  return trial.entities.filter((e) => e.entitiesTypeKey == entitiesTypeKey);
+};
+
+export const getEntitiesTypeArrayFromSelectedTrial = (trial, entitiesTypes) => {
+  const entitiesTypeKeyArr = trial.entities.map((obj) => {
+    return obj.entitiesTypeKey;
+  });
+
+  return Object.entries(entitiesTypes)
+    .filter((e) => entitiesTypeKeyArr.indexOf(e[0]) != -1)
+    .map((item) => item[1][0]);
 };
