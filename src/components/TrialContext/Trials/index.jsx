5d79d1e36338b6093b8f822ac62397478eb8e3e1
saraw <saraw@linnovate.net>
2022-06-06 11:38:05 +0300

    Positions of devices for new trials in the map

--- src/components/TrialContext/Trials/index.jsx
+++ src/components/TrialContext/Trials/index.jsx
@@ -315,9 +315,7 @@ class Trials extends React.Component {
               withBackButton
               backButtonHandler={() => history.push(`/experiments/${match.params.id}/${TRIAL_SETS_DASH}`)}
               rightDescription={trialSet ? trialSet.name : ''}
-              addButtonHandler={() => history.push(
-                `/experiments/${match.params.id}/${TRIAL_SETS_DASH}/${match.params.trialSetKey}/add-trial`,
-              )}
+              addButtonHandler={() => window.location.href = `/experiments/${match.params.id}/${TRIAL_SETS_DASH}/${match.params.trialSetKey}/add-trial`}
             />
             <ContentTable
               contentType={TRIALS}
