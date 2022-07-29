beb8d71f8cf2993400306373bb83b55c4b0c4a9c
Ori Menachem Levi <oriml@kaplanopensource.co.il>
2022-07-29 18:20:32 +0300

    styling edit entity table tool

--- src/components/EntityPlanner/new/ToBePositionTable/EditTable/ToolsBar/EditEntity.jsx
+++ src/components/EntityPlanner/new/ToBePositionTable/EditTable/ToolsBar/EditEntity.jsx
@@ -1,8 +1,111 @@
-import React from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import DeleteIcon from '@material-ui/icons/Delete';
+import { IconButton, InputAdornment, TextField } from '@material-ui/core';
+import { Button } from './Button';
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 350,
+  },
+  inputField: {
+    width: 150,
+    height: 30,
+    '&>*': {
+      width: 'inherit',
+      height: 'inherit',
+    }
+  },
+  inputPositionField: {
+    width: 50,
+    height: 30,
+    height: 'inherit',
+    '&>*': {
+      width: 'inherit',
+      height: 'inherit',
+    }
+  }
+});
+
+function createData(name, type, weight, height, positionX, positionY, icon) {
+  return { name, type, weight, height, positionX, positionY, icon };
+}
+
+export function EditEntityTool() {
+
+  const classes = useStyles();
+
+  const rows = [
+    createData(
+      <TextField className={classes.inputField} defaultValue="Entity 1" variant="outlined" />,
+      <TextField className={classes.inputField} defaultValue="Samsung" variant="outlined" />,
+      <TextField className={classes.inputField} defaultValue="20kg" variant="outlined" />,
+      <TextField className={classes.inputField} defaultValue="50cm" variant="outlined" />,
+      <TextField
+        className={classes.inputPositionField}
+        InputProps={{
+          startAdornment: <InputAdornment position="start">x</InputAdornment>,
+        }}
+      />,
+      <TextField
+        className={classes.inputPositionField}
+        InputProps={{
+          startAdornment: <InputAdornment position="start">y</InputAdornment>,
+        }}
+      />,
+      <IconButton><DeleteIcon /></IconButton>
+    ),
+  ];
+
+
+  return (
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell> Name </TableCell>
+            <TableCell> Type </TableCell>
+            <TableCell> Weight </TableCell>
+            <TableCell> Height </TableCell>
+            <TableCell> Position </TableCell>
+            <TableCell> {'  '} </TableCell>
+            <TableCell> {'  '} </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            // name, type, weight, height, positionX, positionY, icon
+            <TableRow key={row.name}>
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell>{row.type}</TableCell>
+              <TableCell>{row.weight}</TableCell>
+              <TableCell>{row.height}</TableCell>
+              <TableCell>{row.positionX}</TableCell>
+              <TableCell>{row.positionY}</TableCell>
+              <TableCell>{row.icon}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
 
 const EditEntity = () => {
   return (
-    <div>EditEntity</div>
+    <div>
+      <EditEntityTool />
+      <Button text='save changes' onClick={(() => { })} />
+    </div>
   )
 }
 
