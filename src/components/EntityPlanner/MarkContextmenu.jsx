0a6ccb91dc23f88ef01ecb41d46a2b246ed6fc35
Ori Menachem Levi <oriml@kaplanopensource.co.il>
2022-08-11 15:39:55 +0300

    #257 create add/remove entity (from TBP table on map) popup after right click on entity on map

--- src/components/EntityPlanner/MarkContextmenu.jsx
+++ src/components/EntityPlanner/MarkContextmenu.jsx
@@ -1,36 +1,62 @@
-import React from 'react'
-import { DomEvent } from 'leaflet'
-export const MarkContextmenu = ({ menuRows,isShow, position, onClose}) =>{
+import React from 'react';
+import { DomEvent } from 'leaflet';
+import { makeStyles } from "@material-ui/core/styles";
 
+const style = {
+    container: {
+
+        position: 'absolute',
+        zIndex: 1999,
+        backgroundColor: 'white',
+        fontFamily: 'Inter',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'flex-start',
+        background: '#FFFFFF',
+        boxShadow: '0px 10px 10px rgba(0, 0, 0, 0.04), 0px 20px 25px rgba(0, 0, 0, 0.1)',
+    },
+    row: {
+        width: '100%',
+        cursor: 'pointer',
+        padding: '10px 14px',
+        '&:hover': {
+            background: '#DDF5FC',
+        }
+
+    }
+}
+const useStyles = makeStyles(style)
+
+export const MarkContextmenu = ({ menuRows, isShow, position, onClose }) => {
+    const classes = useStyles();
     const divRef = React.useRef(null);
 
     React.useEffect(() => {
-    if(divRef.current)
-      DomEvent.disableClickPropagation(divRef.current);
+        if (divRef.current)
+            DomEvent.disableClickPropagation(divRef.current);
     });
 
     return (
-    isShow &&
-    <div style={{
-        position: 'absolute',
-        top: position.y,
-        left: position.x,
-        zIndex: 1999,
-        border: '2px solid black',
-        backgroundColor: 'white',
-        display: 'flex',
-        flexDirection: 'column'
-        }}
-        ref={divRef}
-        onClick={onClose}
+        isShow &&
+        <div 
+            className={classes.container}
+            style={{top: position.y,left: position.x,}}
+            ref={divRef}
+            onClick={onClose}
         >
-        {
-            menuRows.map(({onClick, text}) => (
-                <span onClick={(e) => {onClick(); console.log(e)}}>{text}</span>
-            ))
-        }
-        <span>Cancel</span>
-    </div>
-  )
+            {
+                menuRows.map(({ onClick, text }) => (
+                    <span
+                    className={classes.row}
+                    onClick={(e) => { onClick(); console.log(e) }}
+                    >
+                    {text}
+                    </span>
+                ))
+            }
+            <span  className={classes.row}>Cancel</span>
+        </div>
+    )
 
-    }
+}
