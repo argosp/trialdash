b35671f633e6ea000c0e87dfb02772a7ea4c1ba1
Ori Menachem Levi <oriml@kaplanopensource.co.il>
2022-08-16 09:24:39 +0300

    change Popup marker style

--- src/components/EntityPlanner/EntityMarker.jsx
+++ src/components/EntityPlanner/EntityMarker.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Marker, Popup } from "react-leaflet";
 import { divIcon } from 'leaflet';
 import { renderToStaticMarkup } from 'react-dom/server';
+import { ReactComponent as CellTowerIcon } from "./CellTowerIcon.svg";
 
 export const EntityMarker = ({ entity, devLocation, isSelected, isTypeSelected, isOnEdit, shouldShowName, handleMarkerClick, onContextMenu }) => (
     <Marker key={entity.name}
@@ -28,8 +29,49 @@ export const EntityMarker = ({ entity, devLocation, isSelected, isTypeSelected, 
             )
         })}
     >
-        <Popup>
-            {entity.name + ' at (' + devLocation + ')'}
+        <Popup position={[devLocation[0] + 0.0008, devLocation[1]]}>
+            <div style={{ display: "flex", height: "4vh" }}>
+                <div
+                    style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        height: "100%",
+                        width: "10%",
+                        alignItems: "center",
+                        marginRight: "15px",
+                    }}
+                >
+                    <CellTowerIcon />
+                </div>
+                <div style={{}}>
+                    <div
+                        style={{
+                            display: "flex",
+                            alignItems: "center",
+                            height: "50%",
+                            fontFamily: "Inter",
+                            fontStyle: "normal",
+                            fontWeight: 600,
+                            fontSize: "13px",
+                            lineHeight: "16px",
+                        }}
+                    >
+                        {entity.name}
+                    </div>
+                    <div
+                        style={{
+                            height: "50%",
+                            fontFamily: "Inter",
+                            fontStyle: "normal",
+                            fontWeight: 400,
+                            fontSize: "12px",
+                            lineHeight: "15px",
+                        }}
+                    >{`${Math.round(devLocation[0] * 10000000) / 10000000} X ${Math.round(devLocation[1] * 10000000) / 10000000
+                        } `}</div>
+                </div>
+            </div>
         </Popup>
+
     </Marker >
 )
