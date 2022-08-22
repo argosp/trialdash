78c4fb8e9f8d04ffded14d5d6945d4f8082b88b0
Ori Neeman <orineeman0@gmail.com>
2022-08-22 16:34:44 +0300

    #266 - Design of PopperBox of poly & curve & rect

--- src/components/EntityPlanner/new/ToBePositionTable/EditTable/ToolsBar/DistrubteAlongLine.jsx
+++ src/components/EntityPlanner/new/ToBePositionTable/EditTable/ToolsBar/DistrubteAlongLine.jsx
@@ -1,25 +1,51 @@
 import React from 'react';
-import { Box, Grid, TextField, Typography } from '@material-ui/core';
+import { Grid, TextField, Typography } from '@material-ui/core';
 import { Button } from './Button';
+import processingDecimalDigits from '../../../../utils/processingDecimalDigits';
 
-const DistributeAlongLine = ({ classes, onSubmit }) => {
-  const mock = { x: 23.34546, y: 45.34342 };
+const DistributeAlongLine = ({ classes, onSubmit, markedPoints }) => {
+  const labels = ['Start', 'End'];
+  const positions = [
+    { x: '', y: '' },
+    { x: '', y: '' },
+  ];
+  if (markedPoints.length > 0) {
+    markedPoints.forEach((markedPoint, index) => {
+      const point = { x: markedPoint[0], y: markedPoint[1] };
+      positions[index] = {
+        x: processingDecimalDigits(point.x),
+        y: processingDecimalDigits(point.y),
+      };
+    });
+  }
+
   return (
     <Grid container className={classes.tool}>
-      <Grid item className="toolItem">
-        <TextField
-          id="x-input"
-          label="x"
-          defaultValue={mock.x}
-          InputLabelProps={{ shrink: true }}
-        />
-        <TextField
-          id="y-input"
-          label="y"
-          defaultValue={mock.y}
-          InputLabelProps={{ shrink: true }}
-        />
-      </Grid>
+      {positions.slice(0, 2).map((point, index) => (
+        <Grid item className="toolItem" key={index}>
+          <Grid item md={1}>
+            <Typography component="span">{labels[index]}</Typography>
+          </Grid>
+          <Grid item md={4}>
+            <TextField
+              InputProps={{ style: { fontSize: 14 } }}
+              id="x-input"
+              label="x"
+              value={point.x}
+              InputLabelProps={{ shrink: true }}
+            />
+          </Grid>
+          <Grid item md={4}>
+            <TextField
+              InputProps={{ style: { fontSize: 14 } }}
+              id="y-input"
+              label="y"
+              value={point.y}
+              InputLabelProps={{ shrink: true }}
+            />
+          </Grid>
+        </Grid>
+      ))}
       <Button className="button" text="distribute" onClick={onSubmit} />
     </Grid>
   );
