a32bf8205b68def2ad448517ca156cd752068da0
Ori Neeman <orineeman0@gmail.com>
2022-10-02 13:48:05 +0300

    Creating a new view for many entities located in one point #283

--- src/components/EntityPlanner/EntityMarker.jsx
+++ src/components/EntityPlanner/EntityMarker.jsx
@@ -7,6 +7,7 @@ import processingDecimalDigits from './utils/processingDecimalDigits';
 import { Grid, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { styles } from './styles';
+import { getGroupKey } from './EntityUtils';
 
 const useStyles = makeStyles(styles);
 
@@ -19,9 +20,10 @@ export const EntityMarker = ({
   shouldShowName,
   handleMarkerClick,
   onContextMenu,
+  entityType,
 }) => {
   const classes = useStyles();
-
+  const groupKey = getGroupKey(entity, entityType);
   return (
     <Marker
       key={entity.name}
@@ -36,7 +38,7 @@ export const EntityMarker = ({
           <Typography component={'div'}>
             <i
               className="fas fa-circle fa-lg"
-              style={{ color: isOnEdit ? '#2D9CDB' : '#27AE60' }}
+              style={{ color: isOnEdit ? '#2D9CDB' : groupKey ? '#9B51E0' : '#27AE60' }}
               // save this comment to future develop with groups
               // style={{ color: (isTypeSelected ? (isSelected ? '#9B51E0' : '#2D9CDB') : '#27AE60') }}
             />
