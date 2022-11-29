9c6e5b210620b25426728ee04aae3e88c924bb24
Eran Geva <eran.geva@gmail.com>
2022-11-29 13:59:06 +0200

    remove unneeded fields

--- src/components/ExperimentContext/ExperimentForm/AnchorPoints.jsx
+++ src/components/ExperimentContext/ExperimentForm/AnchorPoints.jsx
@@ -5,15 +5,19 @@ import { DashedPolyline } from "./DashedPolyline.jsx";
 
 
 // const pointLatLngToMeters = (p) => {
-//     const fix = (n) => Math.round(n * 1e9) / 1e9;
 //     return `(${fix(p.lng)}, ${fix(p.lat)}) in meters<br/>(${fix(p.x)}, ${fix(p.y)}) in pixels`;
 // }
 
-export const AnchorPoints = ({ anchorLatLng, horizontalLng, verticalLat, setAnchorLatLng, setHorizontalLng, setVerticalLat }) => {
+export const AnchorPoints = ({ anchorLatLng,
+    horizontalLng, verticalLat,
+    setAnchorLatLng, setHorizontalLng, setVerticalLat,
+    anchorXY, horizontalX, verticalY
+}) => {
     const anchorPoint = [anchorLatLng.lat, anchorLatLng.lng];
     const horizontalPoint = [anchorLatLng.lat, horizontalLng];
     const verticalPoint = [verticalLat, anchorLatLng.lng];
 
+    const fix = (n) => Math.round(n * 1e9) / 1e9;
     // console.log(verticalPoint, anchorPoint, horizontalPoint);
 
     return (
@@ -22,7 +26,7 @@ export const AnchorPoints = ({ anchorLatLng, horizontalLng, verticalLat, setAnch
                 key='anchor'
                 location={anchorPoint}
                 setLocation={p => setAnchorLatLng({ lat: p[0], lng: p[1] })}
-            // locationToShow={pointLatLngToMeters(anchor)}
+                locationToShow={`(${fix(anchorLatLng.lng)}, ${fix(anchorLatLng.lat)}) in meters<br/>(${fix(anchorXY.x)}, ${fix(anchorXY.y)}) in pixels`}
             >
             </MarkedPoint>
             <ChosenMarker
@@ -33,6 +37,7 @@ export const AnchorPoints = ({ anchorLatLng, horizontalLng, verticalLat, setAnch
                 key='horiz'
                 location={horizontalPoint}
                 setLocation={p => setHorizontalLng(p[1])}
+                locationToShow={`(${fix(horizontalLng)}, ${fix(anchorLatLng.lat)}) in meters<br/>(${fix(horizontalX)}, ${fix(anchorXY.y)}) in pixels`}
             // locationToShow={pointLatLngToMeters(horizontalPoint)}
             >
             </MarkedPoint>
@@ -40,6 +45,7 @@ export const AnchorPoints = ({ anchorLatLng, horizontalLng, verticalLat, setAnch
                 key='verti'
                 location={verticalPoint}
                 setLocation={p => setVerticalLat(p[0])}
+                locationToShow={`(${fix(anchorLatLng.lng)}, ${fix(verticalLat)}) in meters<br/>(${fix(anchorXY.x)}, ${fix(verticalY)}) in pixels`}
             // locationToShow={pointLatLngToMeters(verticalPoint)}
             >
             </MarkedPoint>
--- src/components/ExperimentContext/ExperimentForm/MapStandalone.jsx
+++ src/components/ExperimentContext/ExperimentForm/MapStandalone.jsx
@@ -11,53 +11,58 @@ export const MapStandalone = ({ row, setRow }) => {
 
   const imageSize = { x: row.width || 300, y: row.height || 400 };
 
-  const [anchor, setAnchor] = useState({
-    lat: row.lower,
-    lng: row.left,
-    x: 0,
-    y: 0
-  });
+  const [anchor, setAnchor] = useState({ lat: row.lower, lng: row.left, x: 0, y: 0 });
+  const [horizontalPoint, setHorizontalPoint] = useState({ lat: row.lower, lng: row.right, x: imageSize.x, y: 0 });
+  const [verticalPoint, setVerticalPoint] = useState({ lat: row.upper, lng: row.left, x: 0, y: imageSize.y });
 
-  const [distances, setDistances] = useState({
-    x: imageSize.x,
-    y: imageSize.y,
-    lat: row.upper - row.lower,
-    lng: row.right - row.left
-  });
+  // const [distances, setDistances] = useState({
+  //   x: imageSize.x,
+  //   y: imageSize.y,
+  //   lat: row.upper - row.lower,
+  //   lng: row.right - row.left
+  // });
 
   React.useEffect(() => {
     mapRef.current.leafletElement.fitBounds([[row.lower, row.left], [row.upper, row.right]]);
   }, []);
 
-  const dlat = Math.abs(distances.lat) * Math.sign(distances.y);
-  const dlng = Math.abs(distances.lng) * Math.sign(distances.x);
-  const horizontalPoint = { lat: anchor.lat, lng: anchor.lng + dlng, x: anchor.x + distances.x, y: anchor.y };
-  const verticalPoint = { lat: anchor.lat + dlat, lng: anchor.lng, x: anchor.x, y: anchor.y + distances.y };
+  // const dlat = Math.abs(distances.lat) * Math.sign(distances.y);
+  // const dlng = Math.abs(distances.lng) * Math.sign(distances.x);
+  // const horizontalPoint = { lat: anchor.lat, lng: anchor.lng + dlng, x: anchor.x + distances.x, y: anchor.y };
+  // const verticalPoint = { lat: anchor.lat + dlat, lng: anchor.lng, x: anchor.x, y: anchor.y + distances.y };
 
-  React.useEffect(() => {
-    if (Math.min(Math.abs(distances.x), Math.abs(distances.y), Math.abs(distances.lat), Math.abs(distances.lng)) < 1e-6) {
-      return;
-    }
+  // React.useEffect(() => {
+  //   if (Math.min(Math.abs(distances.x), Math.abs(distances.y), Math.abs(distances.lat), Math.abs(distances.lng)) < 1e-6) {
+  //     return;
+  //   }
 
-    const right = Math.round((anchor.lng + dlng / distances.x * (imageSize.x - anchor.x)) * 1e9) / 1e9;
-    const left = Math.round((horizontalPoint.lng - dlng / distances.x * horizontalPoint.x) * 1e9) / 1e9;
-    const lower = Math.round((anchor.lat - dlat / distances.y * anchor.y) * 1e9) / 1e9;
-    const upper = Math.round((verticalPoint.lat + dlat / distances.y * (imageSize.y - verticalPoint.y)) * 1e9) / 1e9;
+  //   const right = Math.round((anchor.lng + dlng / distances.x * (imageSize.x - anchor.x)) * 1e9) / 1e9;
+  //   const left = Math.round((horizontalPoint.lng - dlng / distances.x * horizontalPoint.x) * 1e9) / 1e9;
+  //   const lower = Math.round((anchor.lat - dlat / distances.y * anchor.y) * 1e9) / 1e9;
+  //   const upper = Math.round((verticalPoint.lat + dlat / distances.y * (imageSize.y - verticalPoint.y)) * 1e9) / 1e9;
 
-    if (Math.max(Math.abs(right - row.right), Math.abs(left - row.left), Math.abs(lower - row.lower), Math.abs(upper - row.upper)) < 1e-6) {
-      return;
-    }
+  //   if (Math.max(Math.abs(right - row.right), Math.abs(left - row.left), Math.abs(lower - row.lower), Math.abs(upper - row.upper)) < 1e-6) {
+  //     return;
+  //   }
 
-    // console.log('right', right, row.right);
-    // console.log('left', left, row.left);
-    // console.log('lower', lower, row.lower);
-    // console.log('upper', upper, row.upper);
+  //   // console.log('right', right, row.right);
+  //   // console.log('left', left, row.left);
+  //   // console.log('lower', lower, row.lower);
+  //   // console.log('upper', upper, row.upper);
 
-    setRow(Object.assign({}, row, { lower, right, upper, left }));
-    setTimeout(() => {
-      mapRef.current.leafletElement.fitBounds([[lower, left], [upper, right]]);
-    }, 200);
-  });
+  //   setRow(Object.assign({}, row, { lower, right, upper, left }));
+  //   setTimeout(() => {
+  //     mapRef.current.leafletElement.fitBounds([[lower, left], [upper, right]]);
+  //   }, 200);
+  // });
+
+  const setPointWithoutChange = (setter, lat, lng) => {
+    const x = (lng - row.left) / (row.right - row.left) * imageSize.x;
+    const y = (lat - row.lower) / (row.upper - row.lower) * imageSize.y;
+    setter({ lat, lng, x, y });
+  }
+
+  console.log(anchor, horizontalPoint, verticalPoint);
 
   return (
     <Grid container>
@@ -70,7 +75,7 @@ export const MapStandalone = ({ row, setRow }) => {
           <Grid item>
             Image size: ({imageSize.x} x {imageSize.y}) <br />
           </Grid>
-          <Grid item>
+          {/* <Grid item>
             <InputXY
               name="Anchor" units="meters"
               x={anchor.lng} setX={(num) => setAnchor({ ...anchor, lng: num })}
@@ -83,33 +88,21 @@ export const MapStandalone = ({ row, setRow }) => {
               x={anchor.x} setX={(num) => setAnchor({ ...anchor, x: num })}
               y={anchor.y} setY={(num) => setAnchor({ ...anchor, y: num })}
             ></InputXY>
-          </Grid>
+          </Grid> */}
           <Grid item>
             <InputXY
               name="Span" units="meters"
-              x={distances.lng} setX={(num) => {
-                const lng = Math.abs(num);
-                if (lng > 1e-6) {
-                  const lat = distances.lat / distances.lng * lng;
-                  setDistances({ ...distances, lat, lng });
-                }
-              }}
-              y={distances.lat} setY={(num) => {
-                const lat = Math.abs(num);
-                if (lat > 1e-6) {
-                  const lng = distances.lng / distances.lat * lat;
-                  setDistances({ ...distances, lat, lng });
-                }
-              }}
+              x={horizontalPoint.lng - anchor.lng} setX={(num) => setHorizontalPoint({ ...horizontalPoint, lng: num + anchor.lng })}
+              y={verticalPoint.lat - anchor.lat} setY={(num) => setVerticalPoint({ ...verticalPoint, lat: num + anchor.lat })}
             ></InputXY>
           </Grid>
-          <Grid item>
+          {/* <Grid item>
             <InputXY
               name="Span" units="pixels"
-              x={distances.x} setX={(num) => setDistances({ ...distances, x: num })}
-              y={distances.y} setY={(num) => setDistances({ ...distances, y: num })}
+              x={horizontalPoint.x - anchor.x} setX={(num) => setHorizontalPoint({ ...horizontalPoint, x: num + anchor.x })}
+              y={verticalPoint.y - anchor.y} setY={(num) => setVerticalPoint({ ...verticalPoint, y: num + anchor.y })}
             ></InputXY>
-          </Grid>
+          </Grid> */}
         </Grid>
       </Grid>
       <Grid item xs={9}>
@@ -123,21 +116,12 @@ export const MapStandalone = ({ row, setRow }) => {
             anchorLatLng={{ lat: anchor.lat, lng: anchor.lng }}
             horizontalLng={horizontalPoint.lng}
             verticalLat={verticalPoint.lat}
-            setAnchorLatLng={({ lat, lng }) => {
-              const x = (lng - row.left) / (row.right - row.left) * imageSize.x;
-              const y = (lat - row.lower) / (row.upper - row.lower) * imageSize.y;
-              setAnchor({ lat, lng, x, y });
-            }}
-            setHorizontalLng={lng => {
-              const xmeters = lng - anchor.lng;
-              const x = xmeters / (row.right - row.left) * imageSize.x;
-              setDistances({ ...distances, x, lng: Math.abs(xmeters) })
-            }}
-            setVerticalLat={lat => {
-              const ymeters = lat - anchor.lat;
-              const y = ymeters / (row.upper - row.lower) * imageSize.y;
-              setDistances({ ...distances, y, lat: Math.abs(ymeters) })
-            }}
+            anchorXY={{ x: anchor.x, y: anchor.y }}
+            horizontalX={horizontalPoint.x}
+            verticalY={verticalPoint.y}
+            setHorizontalLng={lng => setPointWithoutChange(setHorizontalPoint, anchor.lat, lng)}
+            setVerticalLat={lat => setPointWithoutChange(setVerticalPoint, lat, anchor.lng)}
+            setAnchorLatLng={({ lat, lng }) => setPointWithoutChange(setAnchor, lat, lng)}
           ></AnchorPoints>
         </MapWithImage>
       </Grid>
