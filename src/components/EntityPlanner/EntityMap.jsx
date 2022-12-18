37bcfdec0e2c1e6cfbb37bd1c1bcaba6742e69b1
Eran Geva <eran.geva@gmail.com>
2022-12-18 14:35:59 +0200

    refactor out GridlinesLayer

--- src/components/EntityPlanner/EntityMap.jsx
+++ src/components/EntityPlanner/EntityMap.jsx
@@ -1,36 +1,14 @@
 import React from 'react';
 import {
   Map as LeafletMap,
-  TileLayer,
   LayersControl,
   ImageOverlay,
   LayerGroup,
-  Polyline,
-  Tooltip,
 } from 'react-leaflet';
 import config from '../../config';
 import { CRS } from 'leaflet';
-import { styled } from '@material-ui/core/styles';
-
-const NudeTooltip = styled(Tooltip)({
-  background: 0,
-  border: 0,
-  borderRadius: 0,
-  boxShadow: 'none',
-  padding: 0,
-  '&.leaflet-tooltip-top:before': {
-    border: 0,
-  },
-  '&.leaflet-tooltip-bottom:before': {
-    border: 0,
-  },
-  '&.leaflet-tooltip-left:before': {
-    border: 0,
-  },
-  '&.leaflet-tooltip-right:before': {
-    border: 0,
-  },
-});
+import { RealMapLayer } from './Map/RealMapLayer';
+import { GridlinesLayer } from './Map/GridlinesLayer';
 
 const position = [32.081128, 34.779729];
 const posbounds = [
@@ -43,20 +21,6 @@ const bounds2arr = (bounds) => {
     [bounds.getNorth(), bounds.getWest()],
     [bounds.getSouth(), bounds.getEast()],
   ];
-};
-
-const RealMapLayer = () => {
-  const mapAttrib =
-    process.env.REACT_APP_MAP_ATTRIBUTION ||
-    '&copy; <a href="https://carto.com">Carto</a> contributors';
-  const mapTileUrl =
-    process.env.REACT_APP_MAP_URL ||
-    'https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}.png';
-  if (!process.env.REACT_APP_MAP_ATTRIBUTION || !process.env.REACT_APP_MAP_URL) {
-    console.log('Getting map tileserver url from hardcoded:', mapTileUrl);
-  }
-
-  return <TileLayer attribution={mapAttrib} url={mapTileUrl} />;
 };
 
 const EmbeddedImageLayer = ({ image }) => (
@@ -77,78 +41,6 @@ const RealMapWithImagesLayer = ({ images }) => (
     ))}
   </>
 );
-
-const createRangeArray = (from, to, delta) => {
-  let ret = [];
-  if (Math.abs(delta) > 0.1) {
-    const back = [];
-    for (let pos = Math.min(-delta, to); pos > from; pos -= delta) {
-      const show = (back.length + 1) % 5 === 0;
-      const thick = show ? 1.2 : 0.5;
-      back.push({ pos, thick, show });
-    }
-    back.push({ pos: from, thick: 2, show: false });
-    back.reverse();
-
-    const zero = from < 0 && to > 0 ? [{ pos: 0, thick: 2, show: false }] : [];
-
-    const forw = [];
-    for (let pos = Math.max(delta, from); pos < to; pos += delta) {
-      const show = (forw.length + 1) % 5 === 0;
-      const thick = show ? 1.2 : 0.5;
-      forw.push({ pos, thick, show });
-    }
-    forw.push({ pos: to, thick: 2.5, show: false });
-
-    ret = back.concat(zero).concat(forw);
-  }
-  return ret;
-};
-
-const GridlinesLayer = ({ from, to, delta = 1 }) => {
-  const lat0 = Math.min(from[0], to[0]);
-  const lat1 = Math.max(from[0], to[0]);
-  const lng0 = Math.min(from[1], to[1]);
-  const lng1 = Math.max(from[1], to[1]);
-  const lats = createRangeArray(lat0, lat1, delta);
-  const lngs = createRangeArray(lng0, lng1, delta);
-  return (
-    <>
-      {lats.map(({ pos: lat, thick, show }) => {
-        return (
-          <Polyline
-            weight={thick}
-            positions={[
-              [lat, lng0],
-              [lat, lng1],
-            ]}>
-            {!show ? null : (
-              <NudeTooltip permanent direction={'bottom'} offset={[0, -(lng1 + lng0) / 2]}>
-                {lat}
-              </NudeTooltip>
-            )}
-          </Polyline>
-        );
-      })}
-      {lngs.map(({ pos: lng, thick, show }) => {
-        return (
-          <Polyline
-            weight={thick}
-            positions={[
-              [lat0, lng],
-              [lat1, lng],
-            ]}>
-            {!show ? null : (
-              <NudeTooltip permanent direction={'bottom'} offset={[-(lat1 + lat0) / 2, 0]}>
-                {lng}
-              </NudeTooltip>
-            )}
-          </Polyline>
-        );
-      })}
-    </>
-  );
-};
 
 const EntityMapLayers = ({ embedded, standalone, showGrid, showGridMeters, layerChosen }) => {
   if (!standalone.length) {
