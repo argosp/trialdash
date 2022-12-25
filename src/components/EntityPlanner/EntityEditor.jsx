3d301feb1e144ca87c055fb14b216f2fc91afc52
Eran Geva <eran.geva@gmail.com>
2022-12-25 18:40:51 +0200

    move ShapeOptions to ShapeContext

--- src/components/EntityPlanner/EntityEditor.jsx
+++ src/components/EntityPlanner/EntityEditor.jsx
@@ -5,7 +5,6 @@ import { EntityList } from './EntityList';
 import { EntityMap } from './EntityMap';
 import { EntityMarker } from './EntityMarker';
 import { changeEntityLocation, getEntityLocation } from './EntityUtils';
-import { arcCurveFromPoints, lerpPoint, rectByAngle, resamplePolyline, splineCurve } from './GeometryUtils';
 import { InputSlider } from './InputSlider';
 import { MarkedShape } from './MarkedShape';
 import { ShapeChooser } from './ShapeChooser';
@@ -22,7 +21,7 @@ export const EntityEditor = ({ entities, setEntities, showOnlyAssigned, setShowO
     const [layerChosen, setLayerChosen] = React.useState('OSMMap');
     const [showGrid, setShowGrid] = React.useState(false);
     const [showGridMeters, setShowGridMeters] = React.useState(1);
-    const { shape, setShape, rectAngle, setRectAngle, rectRows, setRectRows } = useShape();
+    const { shape, setShape, rectAngle, setRectAngle, rectRows, setRectRows, shapeOptions, shapeData } = useShape();
 
     console.log('EntityEditor', layerChosen, entities, showOnlyAssigned, selectedType, showGrid)
 
@@ -52,58 +51,6 @@ export const EntityEditor = ({ entities, setEntities, showOnlyAssigned, setShowO
             setMarkedPoints(markedPoints.concat([currPoint]));
         }
     };
-
-    const shapeOptions = [
-        {
-            name: 'Point',
-            toLine: points => [],
-            toPositions: (points, amount) => amount && points.length ? [points[0]] : []
-        },
-        {
-            name: 'Poly',
-            toLine: points => [points],
-            toPositions: (points, amount) => resamplePolyline(points, amount)
-        },
-        {
-            name: 'Curve',
-            toLine: points => [splineCurve(points, 100)],
-            toPositions: (points, amount) => resamplePolyline(splineCurve(points, 100), amount)
-        },
-        {
-            name: 'Arc',
-            toLine: points => {
-                if (points.length <= 2) return [points];
-                const arc = arcCurveFromPoints(points, 400);
-                return [[points[0], arc[0]], arc];
-            },
-            toPositions: (points, amount) => resamplePolyline(arcCurveFromPoints(points, 400), amount)
-        },
-        {
-            name: 'Rect',
-            toLine: (points, angle = rectAngle) => {
-                const [nw, ne, se, sw] = rectByAngle(points, angle);
-                return [[nw, ne, se, sw, nw]];
-            },
-            toPositions: (points, amount, rows = rectRows, angle = rectAngle) => {
-                if (points.length === 0) return [];
-                const [nw, ne, se, sw] = rectByAngle(points, angle);
-                let ret = [];
-                const cols = Math.ceil(amount / rows);
-                if (rows > 1 && cols > 1) {
-                    for (let y = 0; y < rows; ++y) {
-                        const west = lerpPoint(nw, sw, y / (rows - 1));
-                        const east = lerpPoint(ne, se, y / (rows - 1));
-                        for (let x = 0; x < cols; ++x) {
-                            ret.push(lerpPoint(west, east, x / (cols - 1)));
-                        }
-                    }
-                }
-                return ret;
-            }
-        }
-    ];
-
-    const shapeData = shapeOptions.find(s => s.name === shape);
 
     const handlePutEntities = () => {
         const positions = shapeData.toPositions(markedPoints, selection.length);
--- src/components/EntityPlanner/ShapeContext.jsx
+++ src/components/EntityPlanner/ShapeContext.jsx
@@ -1,12 +1,5 @@
 import React, { createContext, useContext } from 'react'
-
-// export const ShapeContext = createContext({kind: 'Point'});
-// const { Consumer, Provider } = ShapeContext;
-
-// export const ShapeProvider = Provider;
-// export const useShape = () => useContext(ShapeContext);
-
-// import React from 'react'
+import { arcCurveFromPoints, lerpPoint, rectByAngle, resamplePolyline, splineCurve } from './GeometryUtils';
 
 export const ShapeContext = createContext(null)
 
@@ -15,10 +8,63 @@ export const ShapeProvider = ({ children }) => {
     const [rectAngle, setRectAngle] = React.useState(0);
     const [rectRows, setRectRows] = React.useState(3);
 
+    const shapeOptions = [
+        {
+            name: 'Point',
+            toLine: points => [],
+            toPositions: (points, amount) => amount && points.length ? [points[0]] : []
+        },
+        {
+            name: 'Poly',
+            toLine: points => [points],
+            toPositions: (points, amount) => resamplePolyline(points, amount)
+        },
+        {
+            name: 'Curve',
+            toLine: points => [splineCurve(points, 100)],
+            toPositions: (points, amount) => resamplePolyline(splineCurve(points, 100), amount)
+        },
+        {
+            name: 'Arc',
+            toLine: points => {
+                if (points.length <= 2) return [points];
+                const arc = arcCurveFromPoints(points, 400);
+                return [[points[0], arc[0]], arc];
+            },
+            toPositions: (points, amount) => resamplePolyline(arcCurveFromPoints(points, 400), amount)
+        },
+        {
+            name: 'Rect',
+            toLine: (points, angle = rectAngle) => {
+                const [nw, ne, se, sw] = rectByAngle(points, angle);
+                return [[nw, ne, se, sw, nw]];
+            },
+            toPositions: (points, amount, rows = rectRows, angle = rectAngle) => {
+                if (points.length === 0) return [];
+                const [nw, ne, se, sw] = rectByAngle(points, angle);
+                let ret = [];
+                const cols = Math.ceil(amount / rows);
+                if (rows > 1 && cols > 1) {
+                    for (let y = 0; y < rows; ++y) {
+                        const west = lerpPoint(nw, sw, y / (rows - 1));
+                        const east = lerpPoint(ne, se, y / (rows - 1));
+                        for (let x = 0; x < cols; ++x) {
+                            ret.push(lerpPoint(west, east, x / (cols - 1)));
+                        }
+                    }
+                }
+                return ret;
+            }
+        }
+    ];
+
+    const shapeData = shapeOptions.find(s => s.name === shape);
+
     const store = {
         shape, setShape,
         rectAngle, setRectAngle,
         rectRows, setRectRows,
+        shapeOptions, shapeData
     }
 
     return <ShapeContext.Provider value={store}>
