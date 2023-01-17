bf4bc58b0e8ee959c288bbf0f24d9ff925ac9fad
Eran Geva <eran@kaplanopensource.co.il>
2023-01-17 13:45:56 +0200

    typo

--- src/components/EntityPlanner/EntityTypeRow.jsx
+++ src/components/EntityPlanner/EntityTypeRow.jsx
@@ -5,11 +5,11 @@ import Box from '@material-ui/core/Box'
 import { VisibilityOffOutlined, VisibilityOutlined } from '@material-ui/icons'
 import { Table, TableContainer, TableHead, TableCell, TableRow, TableBody } from '@material-ui/core'
 
-const VisibilityButton = ({ isVisible, setIsVisible }) => {
+const VisibilityButton = ({ isVisible, setIsVisible }) => (
     <IconButton onClick={() => setIsVisible(!isVisible)}>
         {!!isVisible ? <VisibilityOutlined /> : <VisibilityOffOutlined />}
     </IconButton>
-}
+)
 
 export const EntityTypeRow = ({ entity, isVisible, setIsVisible, numberPositioned, numberNotPositioned }) => {
     return (
