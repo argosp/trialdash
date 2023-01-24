ec031b9cd15afca319ab809ef8da0dc6d520ade7
Eran Geva <eran@kaplanopensource.co.il>
2023-01-24 12:33:43 +0200

    capitalize names

--- src/components/EntityPlanner/EditTable/utils/constants.js
+++ src/components/EntityPlanner/EditTable/utils/constants.js
@@ -6,20 +6,20 @@ export const LOCATIONS_MODE = 'locations';
 
 // TOOLS BAR MODES
 export const FREEPOSITIONING_TITLE = 'Free Positioning';
-export const POINT_TITLE = 'point';
-export const CURVE_TITLE = 'curve';
+export const POINT_TITLE = 'Point';
+export const CURVE_TITLE = 'Curve';
 export const DISTRUBTE_ALONG_LINE_TITLE = 'Distribute Along Line';
-export const RECTANGLE_TITLE = 'rectangle';
+export const RECTANGLE_TITLE = 'Rectangle';
 export const SQUARE_TITLE = 'Square';
 export const CLONE_TRIALS_TITLE = 'Clone Trials';
 export const EDIT_ENTITY_TITLE = 'Edit Entity';
 
 // TOOLS BAR VALUES/SHAPE-MODE
-export const FREEPOSITIONING_SHAPE = 'free'; // tmp value. Change after implementation of this tools
-export const POINT_SHAPE = 'point';
-export const CURVE_SHAPE = 'curve';
-export const DISTRUBTE_ALONG_LINE_SHAPE = 'poly';
-export const RECTANGLE_SHAPE = 'rect';
+export const FREEPOSITIONING_SHAPE = 'Pop'; // tmp value. Change after implementation of this tools
+export const POINT_SHAPE = 'Point';
+export const CURVE_SHAPE = 'Curve';
+export const DISTRUBTE_ALONG_LINE_SHAPE = 'Poly';
+export const RECTANGLE_SHAPE = 'Rect';
 export const SQUARE_SHAPE = 'Square';
 export const CLONE_TRIALS_SHAPE = ''; // tmp value. Change after implementation of this tools
 export const EDIT_ENTITY_SHAPE = ''; // tmp value. Change after implementation of this tools
