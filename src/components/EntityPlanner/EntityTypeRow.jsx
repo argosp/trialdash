c4dce49a2aa90002ea603c83b7050e25f446477c
Eran Geva <eran@kaplanopensource.co.il>
2023-02-28 18:31:25 +0200

    avoid map click on buttons

--- src/components/EntityPlanner/EntityTypeRow.jsx
+++ src/components/EntityPlanner/EntityTypeRow.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import IconButton from '@material-ui/core/IconButton'
 import { VisibilityOffOutlined, VisibilityOutlined, ArrowForwardIos, Clear } from '@material-ui/icons'
 import { TableCell, TableRow } from '@material-ui/core'
+import { DomEvent } from 'leaflet';
 
 export const EntityTypeRow = ({ entity, isVisible, setIsVisible, numberPositioned, numberNotPositioned, setIsOpenArrow, isOpenArrow }) => {
     return (
@@ -13,12 +14,22 @@ export const EntityTypeRow = ({ entity, isVisible, setIsVisible, numberPositione
                 {numberPositioned} / {numberPositioned + numberNotPositioned}
             </TableCell>
             <TableCell padding='none' align="right">
-                <IconButton style={{ padding: 0 }} onClick={() => setIsVisible(!isVisible)}>
+                <IconButton style={{ padding: 0 }}
+                    onClick={e => {
+                        DomEvent.stop(e);
+                        setIsVisible(!isVisible);
+                    }}
+                >
                     {isVisible ? <VisibilityOutlined /> : <VisibilityOffOutlined />}
                 </IconButton>
             </TableCell>
             <TableCell padding='none' align="right">
-                <IconButton style={{ padding: 0 }} onClick={() => setIsOpenArrow(!isOpenArrow)}>
+                <IconButton style={{ padding: 0 }}
+                    onClick={e => {
+                        DomEvent.stop(e);
+                        setIsOpenArrow(!isOpenArrow);
+                    }}
+                >
                     {isOpenArrow ? <Clear /> : <ArrowForwardIos />}
                 </IconButton>
             </TableCell>
