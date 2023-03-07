afec791ca4e94551d67c0bd872e59cdc20523083
Eran Geva <eran@kaplanopensource.co.il>
2023-03-07 12:37:55 +0200

    change TrialEntities to function

--- src/components/TrialContext/TrialForm/TrialEntities/TrialEntities.jsx
+++ src/components/TrialContext/TrialForm/TrialEntities/TrialEntities.jsx
@@ -1,113 +1,131 @@
 /* eslint-disable prefer-destructuring */
-import React from 'react';
-import { withStyles } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
 import { compose } from 'recompose';
 import { withRouter } from 'react-router-dom';
 import { withApollo } from 'react-apollo';
 import { groupBy } from 'lodash';
 import entitiesTypesQuery from '../../../EntityContext/utils/entityTypeQuery';
 import entitiesQuery from '../../../EntityContext/Entities/utils/entityQuery';
-import { styles } from '../styles';
 import EntityPlanner from '../../../EntityPlanner/EntityPlanner';
 import CloneEntitiesDialog from '../../../CloneEntitiesDialog';
 import experimentsQuery from '../../../ExperimentContext/utils/experimentsQuery';
 
-class TrialEntities extends React.Component {
-    state = {
-        trialEntities: {},
+const TrialEntities = ({
+    client,
+    match,
+    showFooter,
+    trial,
+    updateLocation,
+    submitTrial,
+}) => {
+    const [state, setState] = useState({
+        // trialEntities: {},
         entities: {},
         entitiesTypes: {},
-        isLoading: true,
-        parentEntity: {},
-        parentLocation: '',
-        CloneEntitiesDialogOpen: false
-    };
+        // isLoading: true,
+        // parentEntity: {},
+        experimentDataMaps: []
+    });
 
-    componentDidMount() {
+    const entitiesField = trial.status === 'deploy' ? 'deployedEntities' : 'entities';
+
+    const cloneEntitiesRef = React.createRef();
+
+    useEffect(() => {
         (async () => {
-            const { client, match, showFooter } = this.props;
             const data = await client.query({ query: entitiesTypesQuery(match.params.id) });
             const entitiesTypes = groupBy(data.data.entitiesTypes, 'key');
-            let entities = [];
             const entitiesData = await client.query({ query: entitiesQuery(match.params.id) });
-            entities = entitiesData.data.entities;
-            this.setState({
+            const entities = groupBy(entitiesData.data.entities, 'key');
+            // this.orderEntities();
+            const experiments = await client.query({ query: experimentsQuery });
+            const currentExperiment = (experiments.data.experimentsWithData || []).find(experiment => experiment.project.id === trial.experimentId);
+            const experimentDataMaps = currentExperiment ? currentExperiment.maps : [];
+            setState({
+                ...state,
                 entitiesTypes,
-                entities: groupBy(entities, 'key'),
+                entities,
+                experimentDataMaps,
+                // isLoading: false
             });
-            this.orderEntities();
-            await client.query({ query: experimentsQuery });
-            this.setState({ isLoading: false });
             showFooter(false);
-            this.cloneEntitiesRef = React.createRef();
         })()
-    }
+    }, []);
 
-    componentDidUpdate(prevProps) {
+    // useEffect(() => {
+    //     setState({
+    //         ...state,
+    //         update: true,
+    //         trialEntities: groupBy(trial[entitiesField], 'entitiesTypeKey'),
+    //         entitiesField
+    //     });
+    // }, [trial, entitiesField, trial.status, trial[entitiesField]]);
 
-        const { trial } = this.props;
-        const entitiesField = trial.status === 'deploy' ? 'deployedEntities' : 'entities';
-        if (prevProps.triggerUpdate !== this.props.triggerUpdate || prevProps.trial[entitiesField].length !== this.props.trial[entitiesField].length || prevProps.trial[entitiesField].length !== this.state.length || prevProps.trial.status !== trial.status || this.state.entitiesField !== entitiesField) {
-            this.orderEntities();
-        }
-    }
+    // componentDidUpdate(prevProps) {
+    //     if (prevProps.triggerUpdate !== this.props.triggerUpdate ||
+    //         prevProps.trial[entitiesField].length !== this.props.trial[entitiesField].length ||
+    //         prevProps.trial[entitiesField].length !== this.state.length ||
+    //         prevProps.trial.status !== trial.status ||
+    //         this.state.entitiesField !== entitiesField) {
+    //         this.orderEntities();
+    //     }
+    // }
 
-    orderEntities = () => {
-        const { trial } = this.props;
-        const entitiesField = trial.status === 'deploy' ? 'deployedEntities' : 'entities';
-        this.setState({
-            update: true,
-            length: trial[entitiesField].length,
-            trialEntities: groupBy(trial[entitiesField], 'entitiesTypeKey'),
-            entitiesField
-        });
-    }
+    // orderEntities = () => {
+    //     this.setState({
+    //         update: true,
+    //         length: trial[entitiesField].length,
+    //         trialEntities: groupBy(trial[entitiesField], 'entitiesTypeKey'),
+    //         entitiesField
+    //     });
+    // }
 
-    render() {
+    // render() {
 
-        const {
-            match,
-            trial,
-            updateLocation,
-            submitTrial,
-            client,
-        } = this.props;
-        const {
-            entities,
-            entitiesTypes,
-            isLoading,
-            trialEntities,
-        } = this.state;
-        const experiments = !isLoading
-            ? client.readQuery({ query: experimentsQuery }).experimentsWithData
-            : [];
-        const currentExperiment = experiments ? experiments.find(experiment => experiment.project.id === trial.experimentId) : '';
-        return (
-            <EntityPlanner
-                updateLocation={updateLocation}
-                trial={trial}
-                trialEntities={trial[trial.status === 'deploy' ? 'deployedEntities' : 'entities']}
-                allEntities={entities}
-                entitiesTypes={entitiesTypes}
-                experimentDataMaps={currentExperiment ? currentExperiment.maps : []}
-                submitTrial={(updateTrial) => submitTrial(updateTrial)}
-                cloneEntitiesDialog={
-                    <CloneEntitiesDialog
-                        title={"Clone trial"}
-                        ref={this.cloneEntitiesRef}
-                        onConfirm={(updateTrial) => submitTrial(updateTrial)}
-                        entitiesTypes={entitiesTypes}
-                        trialEntities={trialEntities}
-                        currentTrial={trial}
-                        client={client}
-                        match={match}
-                    >
-                    </CloneEntitiesDialog>
-                }
-                openCloneEntitiesDialog={() => this.cloneEntitiesRef.current.openDialog()}
-            />
-        );
-    }
+    const {
+        entities,
+        entitiesTypes,
+        // isLoading,
+        // trialEntities,
+        experimentDataMaps,
+    } = state;
+
+    const trialEntities = groupBy(trial[entitiesField], 'entitiesTypeKey');
+
+    // let experimentDataMaps = [];
+    // if (!isLoading) {
+    //     const _experimentsQuery = client.readQuery({ query: experimentsQuery });
+    //     experiments = _experimentsQuery.experimentsWithData;
+    //     console.log(experiments);
+    // }
+
+    // const currentExperiment = experiments ? experiments.find(experiment => experiment.project.id === trial.experimentId) : '';
+
+    return (
+        <EntityPlanner
+            updateLocation={updateLocation}
+            trial={trial}
+            trialEntities={trial[trial.status === 'deploy' ? 'deployedEntities' : 'entities']}
+            allEntities={entities}
+            entitiesTypes={entitiesTypes}
+            experimentDataMaps={experimentDataMaps}
+            submitTrial={(updateTrial) => submitTrial(updateTrial)}
+            cloneEntitiesDialog={
+                <CloneEntitiesDialog
+                    title={"Clone trial"}
+                    ref={cloneEntitiesRef}
+                    onConfirm={(updateTrial) => submitTrial(updateTrial)}
+                    entitiesTypes={entitiesTypes}
+                    trialEntities={trialEntities}
+                    currentTrial={trial}
+                    client={client}
+                    match={match}
+                >
+                </CloneEntitiesDialog>
+            }
+            openCloneEntitiesDialog={() => cloneEntitiesRef.current.openDialog()}
+        />
+    );
 }
 
 export default compose(
