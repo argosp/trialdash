f0f4c166d1e6aa8b699ec58fbfe6fade6396682c
Eran Geva <eran@kaplanopensource.co.il>
2023-03-07 12:47:53 +0200

    cleanup

--- src/components/TrialContext/TrialForm/TrialEntities/TrialEntities.jsx
+++ src/components/TrialContext/TrialForm/TrialEntities/TrialEntities.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable prefer-destructuring */
 import React, { useEffect, useState } from 'react';
 import { compose } from 'recompose';
 import { withRouter } from 'react-router-dom';
@@ -19,11 +18,8 @@ const TrialEntities = ({
     submitTrial,
 }) => {
     const [state, setState] = useState({
-        // trialEntities: {},
         entities: {},
         entitiesTypes: {},
-        // isLoading: true,
-        // parentEntity: {},
         experimentDataMaps: []
     });
 
@@ -37,69 +33,27 @@ const TrialEntities = ({
             const entitiesTypes = groupBy(data.data.entitiesTypes, 'key');
             const entitiesData = await client.query({ query: entitiesQuery(match.params.id) });
             const entities = groupBy(entitiesData.data.entities, 'key');
-            // this.orderEntities();
             const experiments = await client.query({ query: experimentsQuery });
-            const currentExperiment = (experiments.data.experimentsWithData || []).find(experiment => experiment.project.id === trial.experimentId);
+            const experimentsWithData = (experiments.data.experimentsWithData || []);
+            const currentExperiment = experimentsWithData.find(experiment => experiment.project.id === trial.experimentId);
             const experimentDataMaps = currentExperiment ? currentExperiment.maps : [];
             setState({
                 ...state,
                 entitiesTypes,
                 entities,
                 experimentDataMaps,
-                // isLoading: false
             });
             showFooter(false);
         })()
     }, []);
 
-    // useEffect(() => {
-    //     setState({
-    //         ...state,
-    //         update: true,
-    //         trialEntities: groupBy(trial[entitiesField], 'entitiesTypeKey'),
-    //         entitiesField
-    //     });
-    // }, [trial, entitiesField, trial.status, trial[entitiesField]]);
-
-    // componentDidUpdate(prevProps) {
-    //     if (prevProps.triggerUpdate !== this.props.triggerUpdate ||
-    //         prevProps.trial[entitiesField].length !== this.props.trial[entitiesField].length ||
-    //         prevProps.trial[entitiesField].length !== this.state.length ||
-    //         prevProps.trial.status !== trial.status ||
-    //         this.state.entitiesField !== entitiesField) {
-    //         this.orderEntities();
-    //     }
-    // }
-
-    // orderEntities = () => {
-    //     this.setState({
-    //         update: true,
-    //         length: trial[entitiesField].length,
-    //         trialEntities: groupBy(trial[entitiesField], 'entitiesTypeKey'),
-    //         entitiesField
-    //     });
-    // }
-
-    // render() {
-
     const {
         entities,
         entitiesTypes,
-        // isLoading,
-        // trialEntities,
         experimentDataMaps,
     } = state;
 
     const trialEntities = groupBy(trial[entitiesField], 'entitiesTypeKey');
-
-    // let experimentDataMaps = [];
-    // if (!isLoading) {
-    //     const _experimentsQuery = client.readQuery({ query: experimentsQuery });
-    //     experiments = _experimentsQuery.experimentsWithData;
-    //     console.log(experiments);
-    // }
-
-    // const currentExperiment = experiments ? experiments.find(experiment => experiment.project.id === trial.experimentId) : '';
 
     return (
         <EntityPlanner
