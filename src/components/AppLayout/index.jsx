327b5a039a2c525a4212b46a18642a95e24562ed
Eran Geva <eran@kaplanopensource.co.il>
2023-03-21 09:55:50 +0200

    LinearProgress when working

--- src/components/AppLayout/index.jsx
+++ src/components/AppLayout/index.jsx
@@ -1,43 +1,50 @@
 import React, { createContext, useState } from 'react';
 import { withStyles } from '@material-ui/core/styles';
+import { LinearProgress } from '@material-ui/core';
 import { withRouter } from 'react-router-dom';
 import { compose } from 'recompose';
 import { styles } from './styles';
 import Header from '../Header';
-import LoadingOverlay from 'react-loading-overlay';
+// import LoadingOverlay from 'react-loading-overlay';
 
 export const WorkingContext = createContext();
 
 const AppLayout = ({ classes, children }) => {
   const [working, setWorking] = useState(false);
   return (
-    <LoadingOverlay
-      active={working}
-      spinner
-      text='Please wait...'
-      styles={{
-        overlay: (base) => ({
-          ...base,
-          top: '35vh',
-          height: '30vh',
-          left: '35vw',
-          width: '30vw',
-          position: 'absolute',
-          borderRadius: 20
-        })
-      }}
-    >
-      {/* <div
-        style={{ bottom: 0, top: 0, height: '100vh' }}
-      > */}
+    <>
+      {working
+        ? <LinearProgress style={{zIndex: 1000}} />
+        : null
+      }
       <WorkingContext.Provider value={{ working, setWorking }}>
         <Header />
         <div className={classes.contentWrapper}>
           {children}
         </div>
       </WorkingContext.Provider>
-      {/* </div> */}
-    </LoadingOverlay>
+    </>
+    // <LoadingOverlay
+    //   active={working}
+    //   spinner
+    //   text='Please wait...'
+    //   styles={{
+    //     overlay: (base) => ({
+    //       ...base,
+    //       top: '35vh',
+    //       height: '30vh',
+    //       left: '35vw',
+    //       width: '30vw',
+    //       position: 'absolute',
+    //       borderRadius: 20
+    //     })
+    //   }}
+    // >
+    // {/* <div
+    //   style={{ bottom: 0, top: 0, height: '100vh' }}
+    // > */}
+    // {/* </div> */}
+    // </LoadingOverlay>
   );
 }
 
