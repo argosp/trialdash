12c6aa4880e271c5b09312b9e7a68a686ea331d0
Alex Bondarev <alex@linnovate.net>
2023-03-23 09:40:01 +0300

    Vite config POC

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App />,
+)
+
+serviceWorker.unregister();
---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), svgr()],
+  envPrefix: 'REACT_APP_',
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true, // needed for the Docker Container port mapping to work
+    strictPort: true,
+    port: 3000,
+  }
+})
