4e08a52e23465a216f0e3375c0a18e7b3b0d55e7
Eran Geva <eran@kaplanopensource.co.il>
2023-03-30 13:47:04 +0300

    building prod vite on build folder same like react-script

--- vite.config.js
+++ vite.config.js
@@ -13,5 +13,8 @@ export default defineConfig({
     host: true, // needed for the Docker Container port mapping to work
     strictPort: true,
     port: 3000,
+  },
+  build: {
+    outDir: './build'
   }
 })
