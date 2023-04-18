d4c1762183a8fecd060ad1f6c61e25e0608ab2ea
Eran Geva <eran@kaplanopensource.co.il>
2023-04-18 08:25:28 +0300

    userdata not crush on error

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -36,9 +36,13 @@ const UserData = ({ classes, handleProfileMenuClick }) => (
         `}
   >
     {({ loading, error, data }) => {
-      if (loading) return <p>Loading...</p>;
-      else
-      if (error) return <p> {error}</p>;//TODO check how to set data
+      if (loading) {
+        return <p>Loading...</p>;
+      }
+      if (error) {
+        console.error(error);
+        return <p> {error + ''}</p>;//TODO check how to set data
+      }
       // else if(Object.entries(data).length != 0)
       return (
         <>
