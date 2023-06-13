a96082a02193132e29717fc39d3110b8be0d650d
Eran Geva <eran@kaplanopensource.co.il>
2023-06-13 09:00:10 +0300

    using port from .env

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,26 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), svgr()],
-  envPrefix: 'REACT_APP_',
-  server: {
-    watch: {
-      usePolling: true,
+export default defineConfig(({ command, mode }) => {
+
+  const env = loadEnv(mode, process.cwd(), '')
+  // console.log('mode:', mode, 'cwd:', process.cwd(), 'env:', env);
+  
+  return {
+    plugins: [react(), svgr()],
+    envPrefix: 'REACT_APP_',
+    server: {
+      watch: {
+        usePolling: true,
+      },
+      host: true, // needed for the Docker Container port mapping to work
+      strictPort: true,
+      port: parseInt(env.REACT_APP_PORT), // REACT_APP_PORT on trialdash/.env need to sync with DASHBOARD_PORT on argos/.env
     },
-    host: true, // needed for the Docker Container port mapping to work
-    strictPort: true,
-    port: 3000,
-  },
-  build: {
-    outDir: './build'
-  }
+    build: {
+      outDir: './build'
+    }
+  };
 })
