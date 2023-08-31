f4239c41df707d81e9abdeef94d88e70c96b582e
Eran Geva <eran@kaplanopensource.co.il>
2023-08-31 16:27:51 +0300

    focus on map area when one point

--- src/components/EntityPlanner/RefocusShownEntities.jsx
+++ src/components/EntityPlanner/RefocusShownEntities.jsx
@@ -6,29 +6,28 @@ export const RefocusShownEntities = ({ shownEntityItems }) => {
     const prevShown = usePrevious(shownEntityItems);
     const mapObj = useMap();
     useEffect(() => {
-        const locatedEntities = (shownEntityItems || []).filter(e => {
-            return e.location && e.location.length && isFinite(e.location[0]) && isFinite(e.location[1]);
+        const locatedEntities = (shownEntityItems || []).filter(({ location, isOnLayer }) => {
+            return isOnLayer && location && location.length && isFinite(location[0]) && isFinite(location[1]);
         });
         const hasPrev = prevShown && prevShown.length > 0;
         const hasCurr = locatedEntities && locatedEntities.length > 0;
         if (!hasPrev && hasCurr) {
+            console.log(locatedEntities);
             const lats = locatedEntities.map(({ location }) => location[0]);
             const lngs = locatedEntities.map(({ location }) => location[1]);
 
             const left = Math.min(...lngs);
-            let right = Math.max(...lngs);
-            if (left === right) {
-                right += 0.00001;
-            }
+            const right = Math.max(...lngs);
 
             const lower = Math.min(...lats);
-            let upper = Math.max(...lats);
-            if (upper === lower) {
-                upper += 0.00001;
-            }
+            const upper = Math.max(...lats);
 
-            const bounds = [[upper, left], [lower, right]];
-            mapObj.fitBounds(bounds);
+            if (left === right && upper === lower) {
+                mapObj.panTo([upper, left]);
+            } else {
+                const bounds = [[upper, left], [lower, right]];
+                mapObj.fitBounds(bounds);
+            }
         }
     }, [shownEntityItems])
     return null;
